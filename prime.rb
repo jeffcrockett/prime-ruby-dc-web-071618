bb517ec37001c3ecbaea0b242ba7b1b499ab3239
Jeffrey Crockett <jeffreyneilcrockett@gmail.com>
2018-06-29 23:20:54 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,19 +1,7 @@
 # Add  code here!
 def prime(n)
-  if n <= 1 
-    return false
-  end
-  a = (2..n).map{|i| true}
-  for i in (2..Math.sqrt(n))
-    if a[i]
-      for j in ((i**2 + i)..n)
-        a[j] = false
-      end
-    end
-  end
-  if a.include?(true)
-    return true
-  else 
-    return false 
-  end
+  return false if n <= 1
+  Math.sqrt(n).to_i.downto(2).each {|i| return false if n % i == 0}
+  true
+    
 end
