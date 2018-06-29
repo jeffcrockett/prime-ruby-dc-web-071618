f51a865ac90f05994266f654c87c984d653ed6f6
Jeffrey Crockett <jeffreyneilcrockett@gmail.com>
2018-06-29 23:20:01 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,19 @@
 # Add  code here!
+def prime(n)
+  if n <= 1 
+    return false
+  end
+  a = (2..n).map{|i| true}
+  for i in (2..Math.sqrt(n))
+    if a[i]
+      for j in ((i**2 + i)..n)
+        a[j] = false
+      end
+    end
+  end
+  if a.include?(true)
+    return true
+  else 
+    return false 
+  end
+end
